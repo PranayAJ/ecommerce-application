5b6af9dd14db3eef2ee783b0ee6c259ae9da9acf
PranayAJ <jadhavpranay08@gmail.com>
2022-06-09 14:09:36 +0530

    Brands done

---
+++ app/Models/Attribute.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\AttributeValue;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Attribute
+ * @package App\Models
+ */
+class Attribute extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'attributes';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'code', 'name', 'frontend_type', 'is_filterable', 'is_required'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts  = [
+        'is_filterable' =>  'boolean',
+        'is_required'   =>  'boolean',
+    ];
+
+    public function values()
+    {
+        return $this->hasMany(AttributeValue::class);
+    }
+}
---
+++ app/Models/AttributeValue.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Attribute;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class AttributeValue
+ * @package App\Models
+ */
+class AttributeValue extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'attribute_values';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'attribute_id', 'value', 'price'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'attribute_id'  =>  'integer',
+    ];
+
+    public function attribute()
+    {
+        return $this->belongsTo(Attribute::class);
+    }
+}
